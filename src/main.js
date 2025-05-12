4ea19ae667829b9633a3949fdbf1b3f696d57102
Will IM <vitalii.mieliet@gmail.com>
2025-05-12 22:24:21 +0300

    Add messagr when reach end search

--- src/main.js
+++ src/main.js
@@ -77,7 +77,12 @@ async function handleLodMoreBtnClick() {
 
     const data = await getImagesByQuery(query, page);
     if (page * 15 >= data.totalHits) {
+      page = 1;
       hideElement(loadMoreBtn);
+      iziToast.info({
+        position: 'topRight',
+        message: "We're sorry, but you've reached the end of search results.",
+      });
     }
 
     createGallery(data.hits);
